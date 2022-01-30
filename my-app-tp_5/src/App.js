247f9e718bdaa1f93ac9985d7426fb72e78a1a37
matthieu <matthieu.eccher@efrei.net>
2022-01-30 15:37:54 +0100

    automation test v4

--- my-app-tp_5/src/App.js
+++ my-app-tp_5/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> this is my react docker and jenkins build automation v3? 
+          Edit <code>src/App.js</code> this is my react docker and jenkins build automation v4? 
         </p>
         <a
           className="App-link"
